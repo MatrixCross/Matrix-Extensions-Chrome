4e5e7d77717588efc321791523a808c345431318
Mr.Li <63112788+undercurre@users.noreply.github.com>
2023-01-19 20:46:24 +0800

    ✨新增(typescript): 集成vue

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import presets from './presets/presets';
+
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    plugins: [...presets],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: resolve('./src'),
+            },
+        ],
+    },
+});
